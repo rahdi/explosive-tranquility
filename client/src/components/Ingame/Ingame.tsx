ba63bc024aa4864120d7bdd3c17a293dec666e5b
rahdi <adrian.heiden@gmail.com>
2023-11-14 20:33:54 +0100

    added basic 'ingame' responsiveness

--- client/src/components/Ingame/Ingame.tsx
+++ client/src/components/Ingame/Ingame.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { useLaunchGame } from "./Ingame.utils";
 import { Loader } from "../../shared";
+import "./Ingame.styles.css";
 
 const Ingame = () => {
   const navigate = useNavigate();
@@ -10,7 +11,7 @@ const Ingame = () => {
 
   return (
     <div className="ingame">
-      <div className="ui grid centered">
+      <div className="ui grid centered stackable">
         <div className="three wide column">
           <div
             className="ui right floated secondary button inverted"
